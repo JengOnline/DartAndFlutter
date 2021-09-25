87760dca885bc1facde1fa9d58fa989f4f462cbd
SophonJumpapang <Sophonj@hatari.net>
2021-09-25 17:18:12 +0700

    Spliting the App into Widgets

--- daily_expenses/lib/main.dart
+++ daily_expenses/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:intl/intl.dart';
-import './transaction.dart';
+import './widgets/user_transaction.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,20 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly Froceries',
-      amount: 16.53,
-      date: DateTime.now(),
-    )
-  ];
 
   final titleContoller = TextEditingController();
   final amountController = TextEditingController();
@@ -61,65 +46,7 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-              elevation: 5,
-              child: Container(
-                padding: const EdgeInsets.all(5),
-                child: Column(
-                  children: <Widget>[
-                    TextField(
-                      decoration: const InputDecoration(labelText: 'Title'),
-                      controller: titleContoller,
-                    ),
-                    TextField(
-                      decoration: const InputDecoration(labelText: 'Amount'),
-                      controller: amountController,
-                    ),
-                    TextButton(
-                      style: TextButton.styleFrom(primary: Colors.purple),
-                      child: const Text('Add Transaction'),
-                      onPressed: () {
-                        print(titleContoller.text);
-                      },
-                    )
-                  ],
-                ),
-              )),
-          Column(
-            children: transactions.map((tx) {
-              return Card(
-                  child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.purple, width: 2)),
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      '\$${tx.amount}',
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(tx.title,
-                          style: const TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold)),
-                      Text(DateFormat.yMMMd().format(tx.date),
-                          style: const TextStyle(
-                            color: Colors.grey,
-                          ))
-                    ],
-                  )
-                ],
-              ));
-            }).toList(),
-          )
+          const UserTransaction(),
         ],
       ),
     );
---
+++ daily_expenses/lib/widgets/new_transaction.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final Function addTx;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+  NewTransaction(this.addTx, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: const EdgeInsets.all(5),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: const InputDecoration(labelText: 'Title'),
+              controller: titleController,
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+            ),
+            TextButton(
+              style: TextButton.styleFrom(primary: Colors.purple),
+              child: const Text('Add Transaction'),
+              onPressed: () {
+                addTx(
+                    titleController.text, double.parse(amountController.text));
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
