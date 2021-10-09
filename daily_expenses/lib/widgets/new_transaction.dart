d41a254abafe3d023560840075ca2546e3f7056a
SophonJumpapang <Sophonj@hatari.net>
2021-10-09 16:59:37 +0700

    dynamic padding bottom in new transaction

--- daily_expenses/lib/widgets/new_transaction.dart
+++ daily_expenses/lib/widgets/new_transaction.dart
@@ -49,48 +49,55 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: const EdgeInsets.all(5),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              decoration: const InputDecoration(labelText: 'Title'),
-              controller: _titleController,
-              onSubmitted: (_) => _submitData,
-            ),
-            TextField(
-              decoration: const InputDecoration(labelText: 'Amount'),
-              controller: _amountController,
-              keyboardType: TextInputType.number,
-              onSubmitted: (_) => _submitData,
-            ),
-            Container(
-              height: 70,
-              child: Row(
-                children: <Widget>[
-                  Text(_selectedDate == null
-                      ? 'No Date Chosen!'
-                      : 'Picked Date: ${DateFormat.yMd().format(_selectedDate as DateTime)}'),
-                  TextButton(
-                    child: const Text('Choose Date',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        )),
-                    onPressed: _presentDatePicker,
-                  )
-                ],
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                decoration: const InputDecoration(labelText: 'Title'),
+                controller: _titleController,
+                onSubmitted: (_) => _submitData,
               ),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                  primary: Theme.of(context).textTheme.button?.color,
-                  backgroundColor: Theme.of(context).primaryColor),
-              child: const Text('Add Transaction'),
-              onPressed: _submitData,
-            )
-          ],
+              TextField(
+                decoration: const InputDecoration(labelText: 'Amount'),
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => _submitData,
+              ),
+              Container(
+                height: 70,
+                child: Row(
+                  children: <Widget>[
+                    Text(_selectedDate == null
+                        ? 'No Date Chosen!'
+                        : 'Picked Date: ${DateFormat.yMd().format(_selectedDate as DateTime)}'),
+                    TextButton(
+                      child: const Text('Choose Date',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          )),
+                      onPressed: _presentDatePicker,
+                    )
+                  ],
+                ),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                    primary: Theme.of(context).textTheme.button?.color,
+                    backgroundColor: Theme.of(context).primaryColor),
+                child: const Text('Add Transaction'),
+                onPressed: _submitData,
+              )
+            ],
+          ),
         ),
       ),
     );
