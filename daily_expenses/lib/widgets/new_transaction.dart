937165c1db7beeba245d4c649c9e22e5d423ce10
SophonJumpapang <Sophonj@hatari.net>
2021-10-10 10:12:46 +0700

    use cupartino

--- daily_expenses/lib/widgets/new_transaction.dart
+++ daily_expenses/lib/widgets/new_transaction.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -16,16 +19,21 @@ class _NewTransactionState extends State<NewTransaction> {
   DateTime? _selectedDate;
 
   void _submitData() {
+    if (_amountController.text.isEmpty) {
+      return;
+    }
     final enteredTitle = _titleController.text;
-    final enteredAmount = _amountController.text.isEmpty
-        ? -1
-        : double.parse(_amountController.text);
+    final enteredAmount = double.parse(_amountController.text);
 
     if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
       return;
     }
 
-    widget.addTx(enteredTitle, enteredAmount, _selectedDate);
+    widget.addTx(
+      enteredTitle,
+      enteredAmount,
+      _selectedDate,
+    );
 
     Navigator.of(context).pop();
   }
@@ -60,6 +68,7 @@ class _NewTransactionState extends State<NewTransaction> {
             bottom: MediaQuery.of(context).viewInsets.bottom + 10,
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
               TextField(
                 decoration: const InputDecoration(labelText: 'Title'),
@@ -76,16 +85,28 @@ class _NewTransactionState extends State<NewTransaction> {
                 height: 70,
                 child: Row(
                   children: <Widget>[
-                    Text(_selectedDate == null
-                        ? 'No Date Chosen!'
-                        : 'Picked Date: ${DateFormat.yMd().format(_selectedDate as DateTime)}'),
-                    TextButton(
-                      child: const Text('Choose Date',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          )),
-                      onPressed: _presentDatePicker,
-                    )
+                    Expanded(
+                      child: Text(_selectedDate == null
+                          ? 'No Date Chosen!'
+                          : 'Picked Date: ${DateFormat.yMd().format(_selectedDate as DateTime)}'),
+                    ),
+                    Platform.isIOS
+                        ? CupertinoButton(
+                            child: const Text(
+                              'Choose Date',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            onPressed: _presentDatePicker,
+                          )
+                        : TextButton(
+                            child: const Text('Choose Date',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                )),
+                            onPressed: _presentDatePicker,
+                          )
                   ],
                 ),
               ),
